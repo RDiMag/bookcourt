7db10b893d25025ab6388ca876cd0078cf41b563
RDiMag <87457132+RDiMag@users.noreply.github.com>
2023-01-23 20:25:36 -0500

    updated with contentful packages and review page

---
+++ src/pages/reviews.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+import '/css/style.css'
+import Navbar from "./navbar"
+
+const ReviewPage = () => {
+  return (
+    <>
+     <Navbar />
+
+     <container>
+      <p>Review Page To Come</p>
+     </container>
+
+     </>
+   
+   )
+ }
+    
+
+export default ReviewPage
